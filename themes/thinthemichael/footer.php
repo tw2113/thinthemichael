1e86abbbdd568c5c8007896d06b8afc6c6c65dd1
Michael Beckwith <tw2113@gmail.com>
2020-08-02 13:21:49 -0500

    update tracking code

--- themes/thinthemichael/footer.php
+++ themes/thinthemichael/footer.php
@@ -30,19 +30,23 @@
 	document.querySelector('#quote').innerHTML = random;
 </script>
 <?php } ?>
-<!-- Piwik -->
-<script src="https://trexthepirate.com/traffic/piwik.js" async></script>
-<script>
-	try {
-		var piwikTracker = Piwik.getTracker("https://trexthepirate.com/traffic/piwik.php", 2);
-		piwikTracker.trackPageView();
-		piwikTracker.enableLinkTracking();
-	} catch (err) {}
+
+<!-- Matomo -->
+<script type="text/javascript">
+    var _paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//trexthepirate.com/traffic/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
 </script>
-<noscript><p>
-	<img src="https://trexthepirate.com/traffic/piwik.php?idsite=2" style="border:0" alt="" />
-</p></noscript>
-<!-- End Piwik Tracking Code -->
+<!-- End Matomo Code -->
+
 <?php
 	wp_footer();
 ?>
