1c7954d1f167819596f98f7274079b6ea80e09d0
Michael Beckwith <tw2113@gmail.com>
2021-02-06 17:38:52 -0600

    favicon

--- themes/thinthemichael/functions.php
+++ themes/thinthemichael/functions.php
@@ -200,3 +200,11 @@ function thin_the_add_atom_mime_support( $mimes ) {
 	return $mimes;
 }
 add_filter( 'mime_types', 'thin_the_add_atom_mime_support' );
+
+function thin_the_favicon() {
+	?>
+	<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💪</text></svg>">
+	<?php
+}
+add_action( 'wp_head', 'thin_the_favicon' );
+add_action( 'admin_head', 'thin_the_favicon' );
