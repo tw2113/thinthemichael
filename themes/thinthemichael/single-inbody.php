1bee5351bda746ecc6111e4afb7fb30d6970e162
Michael Beckwith <tw2113@gmail.com>
2023-03-25 11:13:42 -0500

    add some previous scan data to single scan views. for better comparison of progress

--- themes/thinthemichael/single-inbody.php
+++ themes/thinthemichael/single-inbody.php
@@ -15,24 +15,48 @@ get_header();
 					<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 					<p><?php thin_the_posted_on(); ?></p>
 				</header>
-                <ul>
-				<?php
-				$data = get_post_meta( get_the_ID() );
+                <div class="scan-wrapper">
+                    <div class="scan-previous">
+                    <h2><?php esc_html_e( 'Previous Scan', 'thinthemichael' ); ?></h2>
+                    <ul>
+                    <?php
+                        $previous = get_adjacent_post();
+                        $data_prev = get_post_meta( $previous->ID );
+                        echo '<li>' . date( 'm-d-Y', strtotime( $previous->post_date ) ) . '</li>';
+                        foreach( $data_prev as $key => $datum ) {
+                            if ( $key === '_ttm_body_fat_mass' ) {
+                                continue;
+                            }
+                            if ( false === strpos( $key, '_ttm_' ) ) {
+                                continue;
+                            }
 
-				foreach( $data as $key => $datum ) {
-				    if ( $key === '_ttm_body_fat_mass' ) {
-				        continue;
-                    }
-				    if ( false === strpos( $key, '_ttm_' ) ) {
-				        continue;
-                    }
+                            echo format_inbody_meta_line( $key, $datum[0] );
+                        }
+                    ?>
+                    </ul>
+                    </div>
+                    <div class="scan-current">
+	                <h2><?php esc_html_e( 'Current', 'thinthemichael' ); ?></h2>
+                    <ul>
+                    <?php
+                        $data = get_post_meta( get_the_ID() );
+	                    echo '<li>&nbsp;</li>';
+                        foreach( $data as $key => $datum ) {
+                            if ( $key === '_ttm_body_fat_mass' ) {
+                                continue;
+                            }
+                            if ( false === strpos( $key, '_ttm_' ) ) {
+                                continue;
+                            }
 
-				    echo format_inbody_meta_line( $key, $datum[0] );
-                }
-
-				thin_the_posted_in();
-				?>
-                </ul>
+                            echo format_inbody_meta_line( $key, $datum[0] );
+                        }
+                    ?>
+                    </ul>
+                    </div>
+                </div>
+                <?php thin_the_posted_in(); ?>
 			</article>
 			<?php
 			}
