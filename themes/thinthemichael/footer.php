eff126d7264282cc9d7f65414e7adbb288bfa85d
Michael Beckwith <tw2113@gmail.com>
2023-06-04 13:54:12 -0500

    missed quotes

--- themes/thinthemichael/footer.php
+++ themes/thinthemichael/footer.php
@@ -26,7 +26,7 @@
 		'"Come with me if you want to lift"',
 		'"Gonna watch any football tonight?"',
 		'"Let\'s go do lunges"',
-		'Keep your core tight'
+		'"Keep your core tight"'
 	];
 	var random = quotes[Math.floor(Math.random() * quotes.length)];
 	document.querySelector('#quote').innerHTML = random;
