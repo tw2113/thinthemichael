f7f8abc209fabfa880d321d4eb01a1d752d9714b
Michael Beckwith <tw2113@gmail.com>
2023-12-22 15:38:04 -0600

    switch to a more appropriate assets hook

--- themes/kft/functions.php
+++ themes/kft/functions.php
@@ -14,7 +14,8 @@ function kft_styles() {
 		wp_get_theme()->get( 'Version' )
 	);
 }
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\kft_styles' );
+add_action( 'enqueue_block_assets', __NAMESPACE__ . '\kft_styles' );
+
 
 function kft_setup() {
 	add_theme_support( 'wp-block-styles' );
